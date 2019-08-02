cafb1cd2b540aee48428f3ff1db3ef0b0619b8e8
cm <592514568@qq.com>
2019-08-02 11:02:07 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
 
+import 'element-ui/lib/theme-chalk/index.css'
+
+import router from '@/router'
+// 产品级别提示
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
+// 入口文件
+// 职责 引入需要依赖的模块 （js css  包  其他资源）
+// 职责2 创建一个根vue实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
